730d0e44c03c8d4196c958b82ea280be96b016cd
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-03-05 18:28:12 +0100

    rhine: add radish blob
    Change-Id: I3e208ced561725433b091197483c503d87f63d2b

--- rhine-common/rhine-common-vendor-blobs.mk
+++ rhine-common/rhine-common-vendor-blobs.mk
@@ -31,6 +31,7 @@ PRODUCT_COPY_FILES += \
     vendor/sony/rhine-common/proprietary/bin/ptt_socket_app:system/bin/ptt_socket_app \
     vendor/sony/rhine-common/proprietary/bin/qmuxd:system/bin/qmuxd \
     vendor/sony/rhine-common/proprietary/bin/qseecomd:system/bin/qseecomd \
+    vendor/sony/rhine-common/proprietary/bin/radish:system/bin/radish \
     vendor/sony/rhine-common/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/sony/rhine-common/proprietary/bin/sct_service:system/bin/sct_service \
     vendor/sony/rhine-common/proprietary/bin/set_cabc:system/bin/set_cabc \
